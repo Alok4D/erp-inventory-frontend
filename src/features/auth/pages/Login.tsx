7283d3aaa06b4aee6d02199a8028a0eba2c727a2
Alok Roy <alokroy602701@gmail.com>
2026-07-06 21:24:46 +0600

    login mudule api integration complete.

--- src/features/auth/pages/Login.tsx
+++ src/features/auth/pages/Login.tsx
@@ -7,6 +7,7 @@ import { jwtDecode } from 'jwt-decode';
 import { useState } from 'react';
 
 export default function Login() {
+  
   const { register, handleSubmit } = useForm();
   const [login, { isLoading }] = useLoginMutation();
   const dispatch = useAppDispatch();
